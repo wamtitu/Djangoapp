6e39593bf7c2a4fb2a36c67376cf7b7b82fc65f8
wamtitu <wamtitujose@gmail.com>
2023-12-04 18:52:04 +0300

    changes on settings

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
 
-python website/manage.py collectstatic --no-input
-python website/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
--- website/settings.py
+++ website/settings.py
@@ -43,9 +43,9 @@ COSMOSDB = {
 SECRET_KEY = 'django-insecure-@5-vmra4xp^r1%gw&%h)t3ao6&v5g@26lj@wok9%xrzn6r1%no'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://appfile.azurewebsites.net', '127.0.0.1', 'filemanager-0vp2.onrender.com']
 
 
 # Application definition
@@ -139,8 +139,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICfILES_STOROGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_ROOT = BASE_DIR/ 'static'
+STATICfILES_STOROGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR/ 'static'
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
