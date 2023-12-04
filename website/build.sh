32659826fb06788934f852abcadc60be67811972
wamtitu <wamtitujose@gmail.com>
2023-12-04 15:21:36 +0300

    changes

--- website/build.sh
+++ website/build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python website/manage.py collectstatic --no-input
+python webiste/manage.py migrate
