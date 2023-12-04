5b3e51d77d5ceebbf5de441ed0ff19617ec4bd0a
wamtitu <wamtitujose@gmail.com>
2023-12-04 18:54:26 +0300

    forgot about azure

--- website/settings.py
+++ website/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = 'django-insecure-@5-vmra4xp^r1%gw&%h)t3ao6&v5g@26lj@wok9%xrzn6r1%no
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://appfile.azurewebsites.net', '127.0.0.1', 'filemanager-0vp2.onrender.com']
+ALLOWED_HOSTS = ['appfile.azurewebsites.net', '127.0.0.1', 'filemanager-0vp2.onrender.com']
 
 
 # Application definition
